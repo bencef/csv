bf4131474e1bd82af949a887da174a0d2afaccce
Bence Fábián <begnoc@gmail.com>
2020-07-31 09:40:52 +0200

    add nix file

---
+++ default.nix
@@ -0,0 +1,18 @@
+{stdenv, ocamlPackages}:
+
+stdenv.mkDerivation {
+  name = "csv-to-orgtbl";
+  version = "0.1.0";
+  src = ./.;
+
+  buildInputs = with ocamlPackages; [dune ocaml];
+
+  buildPhase = "dune build @all";
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp _build/default/csv_to_orgtbl.exe $out/bin/csv_to_orgtbl
+  '';
+
+  phases = ["unpackPhase" "buildPhase" "installPhase"];
+}
